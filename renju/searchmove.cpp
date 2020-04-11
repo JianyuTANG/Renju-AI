313b354353c359ef337384889055fc7d58f86787
jianyuTANG <jianyutang99@gmail.com>
2020-04-11 12:21:07 +0800

    iteration deepening, with memo

--- renju/searchmove.cpp
+++ renju/searchmove.cpp
@@ -88,10 +88,14 @@ void unregNeighbor2(int x, int y)
 	}
 }
 
-tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth)
+tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth, LINE* mLine, LINE* cLine)
 {
 	if (gameover())
+	{
+		cLine->cmove = 0;
 		return make_tuple(INT32_MIN, 0, 0);
+	}
+		
 	if (depth == 0)
 	{
 		int score = evaluate();
@@ -99,21 +103,34 @@ tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth)
 			score = score;
 		else
 			score = -score;
+		cLine->cmove = 0;
 		return make_tuple(score, 0, 0);
 	}
 
 	int v = INT32_MIN;
 	int ai = 0, aj = 0;
-	for (int i = 1; i <= 14; i++)
+	LINE line;
+
+	if (mLine)
 	{
-		for (int j = 1; j <= 14; j++)
+		int t = 0;
+		while (t < mLine->cmove && mLine->argmove[t] == 0)
 		{
+			t++;
+		}
+		if (t < mLine->cmove)
+		{
+			int pos = mLine->argmove[t];
+			mLine->argmove[t] = 0;
+			int i = pos / 16, j = pos % 16;
+			//cout << pos << endl;
+
 			if (chessBoard[i][j] == blank && statusBoard[i][j])
 			{
 				chessBoard[i][j] = player;
 				regNeighbor2(i, j);
 				turnNum++;
-				auto temp = minValue(nextTurn(player), alpha, beta, depth - 1);
+				auto temp = minValue(nextTurn(player), alpha, beta, depth - 1, mLine, &line);
 				int val = get<0>(temp);
 				if (val > v)
 				{
@@ -130,6 +147,48 @@ tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth)
 					if (v > alpha)
 					{
 						alpha = v;
+						cLine->argmove[0] = (ai << 4) + aj;
+						memcpy(cLine->argmove + 1, line.argmove, line.cmove * sizeof(int));
+						cLine->cmove = line.cmove + 1;
+					}
+				}
+				chessBoard[i][j] = blank;
+				turnNum--;
+				unregNeighbor2(i, j);
+			}
+		}
+		
+	}
+
+	for (int i = 1; i <= 14; i++)
+	{
+		for (int j = 1; j <= 14; j++)
+		{
+			if (chessBoard[i][j] == blank && statusBoard[i][j])
+			{
+				chessBoard[i][j] = player;
+				regNeighbor2(i, j);
+				turnNum++;
+				auto temp = minValue(nextTurn(player), alpha, beta, depth - 1, NULL, &line);
+				int val = get<0>(temp);
+				if (val > v)
+				{
+					v = val;
+					ai = i;
+					aj = j;
+					if (v >= beta)
+					{
+						chessBoard[i][j] = blank;
+						turnNum--;
+						unregNeighbor2(i, j);
+						return make_tuple(v, ai, aj);
+					}
+					if (v > alpha)
+					{
+						alpha = v;
+						cLine->argmove[0] = (ai << 4) + aj;
+						memcpy(cLine->argmove + 1, line.argmove, line.cmove * sizeof(int));
+						cLine->cmove = line.cmove + 1;
 					}
 				}
 				chessBoard[i][j] = blank;
@@ -141,10 +200,14 @@ tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth)
 	return make_tuple(v, ai, aj);
 }
 
-tuple<int, int, int> minValue(int player, int alpha, int beta, int depth)
+tuple<int, int, int> minValue(int player, int alpha, int beta, int depth, LINE* mLine, LINE* cLine)
 {
 	if (gameover())
+	{
+		cLine->cmove = 0;
 		return make_tuple(INT32_MAX, 0, 0);
+	}
+		
 	if (depth == 0)
 	{
 		int score = evaluate();
@@ -152,22 +215,35 @@ tuple<int, int, int> minValue(int player, int alpha, int beta, int depth)
 			score = score;
 		else
 			score = -score;
+		cLine->cmove = 0;
 		return make_tuple(score, 0, 0);
 	}
 		
 
 	int v = INT32_MAX;
 	int ai = 0, aj = 0;
-	for (int i = 1; i <= 14; i++)
+	LINE line;
+
+	if (mLine)
 	{
-		for (int j = 1; j <= 14; j++)
+		int t = 0;
+		while (t < mLine->cmove && mLine->argmove[t] == 0)
 		{
-			if (chessBoard[i][j] == blank && checkNeighbor(i, j))
+			t++;
+		}
+		if (t < mLine->cmove)
+		{
+			int pos = mLine->argmove[t];
+			mLine->argmove[t] = 0;
+			//cout << pos << endl;
+			int i = pos / 16, j = pos % 16;
+
+			if (chessBoard[i][j] == blank && statusBoard[i][j])
 			{
 				chessBoard[i][j] = player;
 				regNeighbor2(i, j);
 				turnNum++;
-				auto temp = maxValue(nextTurn(player), alpha, beta, depth - 1);
+				auto temp = maxValue(nextTurn(player), alpha, beta, depth - 1, mLine, &line);
 				int val = get<0>(temp);
 				if (val < v)
 				{
@@ -184,6 +260,47 @@ tuple<int, int, int> minValue(int player, int alpha, int beta, int depth)
 					if (v < beta)
 					{
 						beta = v;
+						cLine->argmove[0] = (ai << 4) + aj;
+						memcpy(cLine->argmove + 1, line.argmove, line.cmove * sizeof(int));
+						cLine->cmove = line.cmove + 1;
+					}
+				}
+				chessBoard[i][j] = blank;
+				turnNum--;
+				unregNeighbor2(i, j);
+			}
+		}
+	}
+
+	for (int i = 1; i <= 14; i++)
+	{
+		for (int j = 1; j <= 14; j++)
+		{
+			if (chessBoard[i][j] == blank && statusBoard[i][j])
+			{
+				chessBoard[i][j] = player;
+				regNeighbor2(i, j);
+				turnNum++;
+				auto temp = maxValue(nextTurn(player), alpha, beta, depth - 1, NULL, &line);
+				int val = get<0>(temp);
+				if (val < v)
+				{
+					v = val;
+					ai = i;
+					aj = j;
+					if (v <= alpha)
+					{
+						chessBoard[i][j] = blank;
+						turnNum--;
+						unregNeighbor2(i, j);
+						return make_tuple(v, ai, aj);
+					}
+					if (v < beta)
+					{
+						beta = v;
+						cLine->argmove[0] = (ai << 4) + aj;
+						memcpy(cLine->argmove + 1, line.argmove, line.cmove * sizeof(int));
+						cLine->cmove = line.cmove + 1;
 					}
 				}
 				chessBoard[i][j] = blank;
@@ -198,21 +315,31 @@ tuple<int, int, int> minValue(int player, int alpha, int beta, int depth)
 int iterationDeepening(int player)
 {
 	tuple<int, int, int> ans;
+
+	LINE* mLine = NULL, * cLine;
+
 	for (int depth = 1; depth < DEPTH; depth++)
 	{
+		cLine = new LINE;
 		auto start = std::chrono::steady_clock::now();
 
 		// int alpha = INT32_MIN, beta = INT32_MAX;
-		ans = maxValue(player, INT32_MIN, INT32_MAX, depth);
+		ans = maxValue(player, INT32_MIN, INT32_MAX, depth, mLine, cLine);
 
 		auto end = std::chrono::steady_clock::now();
 		auto duration = std::chrono::duration<float, std::milli>(end - start);
+
+		if (mLine)
+			delete mLine;
+		mLine = cLine;
+
 		if (duration.count() > 100)
 		{
 			cout << "timeout: level " << depth << endl;
 			break;
 		}
 	}
+
 	int i = get<1>(ans), j = get<2>(ans), score = get<0>(ans);
 	if (i == 0 && j == 0)
 	{
--- renju/searchmove.h
+++ renju/searchmove.h
@@ -3,10 +3,16 @@
 //ËÑË÷ºËÐÄ
 
 #define DEPTH 6
+#define moveMAX 30
+
+struct LINE {
+	int cmove;            // depth
+	int argmove[moveMAX]; // route
+};
 
 int searchMove();//ËÑË÷ºËÐÄ
-tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth);
-tuple<int, int, int> minValue(int player, int alpha, int beta, int depth);
+tuple<int, int, int> maxValue(int player, int alpha, int beta, int depth, LINE* mLine, LINE* cLine);
+tuple<int, int, int> minValue(int player, int alpha, int beta, int depth, LINE* mLine, LINE* cLine);
 int iterationDeepening(int player);
 
 void regNeighbor(int x, int y);
